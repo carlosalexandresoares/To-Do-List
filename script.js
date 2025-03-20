1f64f2b92081de24932f97c9b65f8349a89b3954
carlosalexandresoares <carlos.soares11@fatec.sp.gov.br>
2025-03-20 18:18:09 -0300

    Finalizado

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 const btn = document.getElementById("addTaskBtn")
 
-btn.addEventListener("click", () =>{
-    let taskInput = document.querySelector("#taskInput").value
+btn.addEventListener("click", () => {
+    let taskInput = document.querySelector("#taskInput")
+    let taskValue = taskInput.value
     const taskList = document.getElementById("taskList")
+
     const li = document.createElement("li")
-    li.innerHTML = `<span>${taskInput}</span>
+    li.innerHTML = `<span>${taskValue}</span>
      <button class="delete-btn">X</button>
     `
 
     li.addEventListener("click", function () {
         li.classList.toggle("completed");
     });
-    if(taskInput === ""){
+    if (taskInput === "") {
         return alert("Adicione algo na lista")
     }
-    li.querySelector(".delete-btn").addEventListener("click", ()=> li.remove());
+    li.querySelector(".delete-btn").addEventListener("click", () => li.remove());
     taskList.appendChild(li);
-    
     taskInput.value = '';
+
 })
